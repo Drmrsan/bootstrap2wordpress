cbf1c70cf886256eb8bedea3cffccd8f808d7e5d
drmrsan <drmrsan@gmail.com>
2015-10-28 01:01:57 +0100

    Added styles and scripts in functions.php

--- functions.php
+++ functions.php
@@ -115,10 +115,16 @@ add_action( 'widgets_init', 'bootstrap2wordpress_widgets_init' );
  */
 function bootstrap2wordpress_scripts() {
 	wp_enqueue_style( 'bootstrap2wordpress-style', get_stylesheet_uri() );
+	wp_enqueue_style( 'bootstrap-style', get_template_directory_uri() . '/assets/css/bootstrap.min.css' );
+	wp_enqueue_style( 'font-awesome-style', get_template_directory_uri() . '/assets/css/font-awesome/css/font-awesome.min.css' );
+	wp_enqueue_style( 'custom-style', get_template_directory_uri() . '/assets/css/custom.css' );
 
 	wp_enqueue_script( 'bootstrap2wordpress-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
 
 	wp_enqueue_script( 'bootstrap2wordpress-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
+
+	wp_enqueue_script( 'bootstrap-javascript-styles', get_template_directory_uri() . '/assets/js/bootstrap.min.js', array('jquery'), '', true );
+	wp_enqueue_script( 'main-js', get_template_directory_uri() . '/assets/js/main.js' );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
@@ -150,3 +156,5 @@ require get_template_directory() . '/inc/customizer.php';
  * Load Jetpack compatibility file.
  */
 require get_template_directory() . '/inc/jetpack.php';
+
+add_filter('show_admin_bar', '__return_false');
--- header.php
+++ header.php
@@ -18,6 +18,13 @@
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
 <?php wp_head(); ?>
+
+<!-- HTML5 shiv and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+
 </head>
 
 <body <?php body_class(); ?>>
@@ -40,7 +47,7 @@
                             <span class="icon-bar"></span>
                             <span class="icon-bar"></span>
                         </button>
-                        <a class="navbar-brand" href="/"><img src="assets/img/logo.png" alt="Bootstrap to WordPress"></a>
+                        <a class="navbar-brand" href="/"><img src="<?php bloginfo('stylesheet_directory') ?>/assets/img/logo.png" alt="Bootstrap to WordPress"></a>
                     </div><!--navbar-header-->
                         
                         <?php 
